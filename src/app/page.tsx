2e1b06d79326bbf39a2a6fae8a5c3e5046ebd444
C10uld <nnn.notnow@gmail.com>
2024-07-05 15:54:03 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <section>
+      <h1 className="text-7xl font-extrabold text-center mt-10">Home Page</h1>
+    </section>
+  );
+};
+
+export default HomePage;
